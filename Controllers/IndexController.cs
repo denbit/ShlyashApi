4d62424962765e916ba70f650d4e79eef2de460c
denbit <den_b_it@i.ua>
2020-01-03 00:31:01 +0200

    added log db

--- Controllers/IndexController.cs
+++ Controllers/IndexController.cs
@@ -3,28 +3,37 @@
 using System;
 using ShlyashApi.Models;
 using System.Data.SQLite;
-using static System.IO.File;
+using DBFile = System.IO.File;
+using System.Text;
+using System.Configuration;
 
-namespace ShlyashApi.Controllers
+namespace ShlyashApi.Controllers 
 {
     public class IndexController : Controller
     {
         public string HealthCheck()
         {
-            return "Mobile and desktop API is online";
+            System.Diagnostics.Debug.WriteLine( String.Join(" ",ConfigurationManager.AppSettings.AllKeys));
+            return  "Mobile and desktop API is online";
         }
         [HttpGet]
         public ActionResult Index()
         {
 
+
             TokenData tokenData = new TokenData
             {
-                client = new MobileClient { Name = "Android", Type = ClientType.Mobile }
-        ,
-                Key="63nummnbynin"
-            };
+                Id = 0,
+                    client = new MobileClient { Name = "Android", Type = ClientType.Mobile },
+                    Key = "63nummnbynin"
+                };
 
             ViewBag.Title = "Registarion";
+            using (var DB = new AccessLogs())
+            {
+                DB.TokenDatas.Add(tokenData);
+                DB.SaveChanges();
+            }
 
             return View("Index", tokenData);
         }
@@ -36,14 +45,30 @@ namespace ShlyashApi.Controllers
         }
         public string Statistics()
         {
-
-            if (!Exists("AccessDB.sqlite"))
+            StringBuilder answer = new StringBuilder();
+            if (!DBFile.Exists(@"C:\Users\Lenovo\source\repos\ShlyashApi\Content\AccessDB.sqlite"))
             {
-                SQLiteConnection.CreateFile("AccessDB.sqlite");
-                return "DB creted"; 
+                SQLiteConnection.CreateFile(@"C:\Users\Lenovo\source\repos\ShlyashApi\Content\AccessDB.sqlite");
+                answer.Append("DB file created"); 
             }
-            else
-                return  "DB not creted";
+                answer.Append( "DB init started");
+
+             SQLiteConnection c=  new SQLiteConnection(@"Data Source=C:\Users\Lenovo\source\repos\ShlyashApi\Content\AccessDB.sqlite;version=3;");
+            c.Open();
+            SQLiteCommand command = new SQLiteCommand();
+            command.Connection = c;
+            command.CommandText = @"CREATE TABLE [workers] (
+                    [id] integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                    [name] char(100) NOT NULL,
+                    [family] char(100) NOT NULL,
+                    [age] int NOT NULL,
+                    [profession] char(100) NOT NULL
+                    );";
+            
+            command.CommandType = System.Data.CommandType.Text;
+            command.ExecuteNonQuery();
+            c.Close();
+            return answer.ToString();
         }
     }
 }
