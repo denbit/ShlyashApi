54830b6ca9755d110f6f34011097e55d1ffde32f
denbit <den_b_it@i.ua>
2020-01-08 00:57:13 +0200

    ext

---
+++ Controllers/LogController.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using ShlyashApi.Models;
+using ShlyashApi.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShlyashApi.Controllers
+{
+    public class LogController : Controller
+    {
+        // GET: Log
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult ViewLog()
+        {
+            Mock<IAccessLogsRepo> mock = new Mock<IAccessLogsRepo>();
+            mock.Setup(m => m.tokenDatas).Returns(new List<TokenData> { new TokenData(), new TokenData() }.AsQueryable());
+            return View(mock.Object);
+        }
+    }
+}
---
+++ Repo/IAccessLogsRepo.cs
@@ -0,0 +1,14 @@
+﻿using ShlyashApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShlyashApi.Repo
+{
+   public  interface IAccessLogsRepo
+    {
+         IQueryable<TokenData> tokenDatas { get; }
+    }
+}
